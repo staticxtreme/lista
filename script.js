66c80f19584928ef93a9e014fe42797bf41cfec3
staticxtreme <gaerstecristofer@gmail.com>
2025-04-28 22:21:00 -0400

    actualizacion del script.js para acceso a supabase y tambien modificacion en el sql para supabase

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
   });
 });
+
+import { createClient } from "@supabase/supabase-js";
+import { supabase } from "./supabaseClient";
+async function fetchData() {
+  const { data, error } = await supabase
+  .from('#myTable')
+  .select('*');
+
+  if (error) {
+    console.error('Error fetching data:', error);
+      console.log('Data:', data);
+  }
+}
+
+fetchData();
+
+function initSupabase() {
+  const supabase = createClient('https://ylndxlqluaofpuzrovne.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsbmR4bHFsdWFvZnB1enJvdm5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5OTI0MTQsImV4cCI6MjA1ODU2ODQxNH0.UohpsTt2BIervDO06RbNLVaE2fnO2D9TGTVYdncwsSo'); //solo existe dentro de la funcion
+}
+initSupabase();
+supabase.from('tabla').select(); //Error: supabase no esta definido
+
+let supabase;
+
+function initSupabase() {
+  supabase = createClient('https://ylndxlqluaofpuzrovne.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsbmR4bHFsdWFvZnB1enJvdm5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5OTI0MTQsImV4cCI6MjA1ODU2ODQxNH0.UohpsTt2BIervDO06RbNLVaE2fnO2D9TGTVYdncwsSo')
+}
+initSupabase();
